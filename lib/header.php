d3e0b0d789f2152b823b835abcb54ad67d0efa90
Kimberly Pennington <kimberlythegeek@gmail.com>
2016-08-24 17:43:34 -0600

    added viewport meta tag

--- lib/header.php
+++ lib/header.php
@@ -14,6 +14,7 @@
     <title> A Code Above | Web Design & Development </title>
     <meta name="web design and development" content="A Code Above | Professional, Aesthetic and Efficient Web Development" />
     <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/> <!--320-->
   </head>
 
   <header>
